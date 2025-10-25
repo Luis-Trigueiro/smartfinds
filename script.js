e36b92a4a6db5ae48372c87c8b834665863f153b
Luis-Trigueiro <marquestrigueiro@gmail.com>
2025-10-25 19:36:33 +0100

    default all

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ function initNavToggle(){
 
 // Init
 render(PRODUCTS);           // render all on load
-filterByCategory();   // then default to tech tab
+//filterByCategory('tech');   // then default to tech tab
 handleNav();
 initNavToggle();
 
